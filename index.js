7c41c1ee08bfa434ab6ab71ac27735a2c679b977
Murilo Fragoso <murilofragoso@exitlag.com>
2025-05-03 16:45:02 -0300

    Adding new pages and audio

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 window.onload = function() {
     createStars();
+    btnFunctions();
 }
 
 
 function createStars() {
-    const numStars = 100;
+    const numStars = 200;
     for (let i = 0; i < numStars; i++) {
       const star = document.createElement('div');
       star.className = 'star';
@@ -13,4 +14,32 @@ function createStars() {
       star.style.animationDuration = (Math.random() * 3 + 2) + 's';
       document.body.appendChild(star);
     }
+}
+
+function btnFunctions() {
+  $(".btn-next").click(function() {
+    let page = parseInt($(this).attr("data-page"));
+    $("#page" + page).animate({opacity: 'toggle'}, function() {
+      let newPage = page + 1;
+      $("#page" + newPage).animate({opacity: 'toggle'});
+      checkParabens(newPage)
+    });
+  })
+
+  $(".btn-back").click(function() {
+    let page = parseInt($(this).attr("data-page"));
+    $("#page" + page).animate({opacity: 'toggle'}, function() {
+      let newPage = page - 1;
+      $("#page" + newPage).animate({opacity: 'toggle'});
+      checkParabens(newPage)
+    });
+  })
+}
+
+function checkParabens(actualPage) {
+  if (actualPage === 5) {
+    $("#audio-parabens")[0].play();
+  } else {
+    $("#audio-parabens")[0].pause();
+  }
 }
